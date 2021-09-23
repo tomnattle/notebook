f804951ca202e544eb7be88e3901ee10c400316f
tomnattle <tomnattle@qq.com>
2021-09-23 16:47:28 +0800

    初始化

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from './logo.svg'
+import tauriCircles from './tauri.svg'
+import tauriWord from './wordmark.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
